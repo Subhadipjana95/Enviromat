e0351260d1994b29e4a905ef5b1cffd42c6a8b11
Subhadip <codesubhadip95@gmail.com>
2025-08-05 21:04:50 +0530

    style(Navbar): update button background colors for consistency
    Update hover and default background colors of buttons in Navbar to maintain visual consistency across the component. The changes ensure a cohesive look and feel when interacting with the navigation elements.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
 
             {/* Icons */}
             <div className="flex items-center space-x-2">
-              <button className="px-2 gap-1 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-gray-400 transition-colors font-[500] text-[#4a5565]">
+              <button className="bg-[#F9FAFB] px-2 gap-1 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-gray-400 transition-colors font-[500] text-[#4a5565]">
                 {/* <Search className="w-4 h-4 text-gray-600" /> */}
                 300
                 <img src="/Coin.png" alt="coin" className="w-7 h-7 text-gray-600" />
               </button>
-              <button className="w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-gray-400 transition-colors">
+              <button className="bg-[#F9FAFB] w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center hover:border-gray-400 transition-colors">
                 <User className="w-5 h-5 text-gray-600" />
               </button>
             </div>
@@ -36,13 +36,13 @@ const Navbar = () => {
             <button className="bg-[#BCD59A] text-gray-700 px-4 py-2 rounded-full text-sm font-medium hover:bg-[#eef0e9] hover:text-[#82a850] border border-[#BCD59A] transition-all duration-[300ms]">
               Services
             </button>
-            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms] hover:bg-gray-200">
+            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms] hover:bg-[#eff8d8]">
               Shop
             </button>
-            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms] hover:bg-gray-200">
+            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms] hover:bg-[#eff8d8]">
               Community
             </button>
-            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms] hover:bg-gray-200">
+            <button className="bg-[#F9FAFB] text-gray-700 hover:text-gray-900 px-4 py-2 rounded-full text-sm font-medium transition-all duration-[300ms]  hover:bg-[#eff8d8]">
               About
             </button>
           </nav>
