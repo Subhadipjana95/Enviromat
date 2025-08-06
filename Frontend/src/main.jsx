11b7dfa615343f0aae7aa73fbacce4969897adb7
subratamondal2 <subrata24012005@gmail.com>
2025-08-06 10:51:37 +0530

    Redux link in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import { Provider } from 'react-redux';
+import rootReducer from "./reducer/index.js";
+
+const store=configureStore({
+  reducer: rootReducer,
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+        <Toaster />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
